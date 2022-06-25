5bade3a97a25d148371e202ebab9a43a2fcbe963
dofranci <dofranci@student.42sp.org.br>
2022-06-25 12:33:05 -0300

    corrections

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dofranci <dofranci@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 19:17:32 by dofranci          #+#    #+#             */
-/*   Updated: 2022/06/16 14:54:31 by dofranci         ###   ########.fr       */
+/*   Updated: 2022/06/25 12:32:52 by dofranci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	size;
 
-	size = ft_strlen(s1);
 	if (s1 == 0 || set == 0)
 		return (0);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
+	size = ft_strlen(s1);
 	while (size && ft_strchr(set, s1[size]))
 		size--;
 	return (ft_substr(s1, 0, size + 1));
