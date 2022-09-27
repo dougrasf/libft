bed30927328e989f490370a0526ae6a1206dc0a1
Douglas F <102628898+dougrasf@users.noreply.github.com>
2022-09-27 20:27:15 -0300

    Update ft_strtrim.c

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -17,30 +17,6 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	/*
-	size_t	size;
-
-	if (s1 == NULL || set == NULL)
-		return (NULL);
-	while (ft_strchr(set, *s1) && *s1)
-		s1++;
-	size = ft_strlen(s1);
-	while (ft_strchr(set, s1[size - 1]))
-		size--;
-	return (ft_substr(s1, 0, size));
-
-
-	size_t	size;
-
-	if (s1 == NULL || set == NULL)
-		return (NULL);
-	while (ft_strchr(set, *s1) && *s1)
-		s1++;
-	size = ft_strlen(s1);
-	while (ft_strchr(set, s1[size]))
-		size--;
-	return (ft_substr(s1, 0, size + 1));
-	*/
 	size_t	size;
 	
 	if (s1 == NULL || set == NULL)
